8aa1e459571aa41e2141fa09a6a4fa04e668e583
aryansharma2k4 <aryansharma2k4@gmail.com>
2025-01-22 11:52:33 +0530

    Added The events banner components

---
+++ src/app/events/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventsBanner from "@/components/EventsBanner";
+
+const Page = () => {
+    return(
+        <>
+            <EventsBanner />
+        </>
+    )
+}
+
+export default Page;
