8a65ab6fd56bbb84a96e88d67fba9193d80f00e5
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2025-02-05 01:58:18 +0530

    updated deletion

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
-        protocol: 'https',
-        hostname: "images.pexels.com"
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
 };
 
 export default nextConfig;
